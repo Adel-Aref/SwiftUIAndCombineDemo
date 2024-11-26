ba47f85fcc6be617d37ee36b815dc778bd71bdbc
adel-aref <adel.aref@mdi.com.eg>
2024-11-27 01:32:43 +0200

    Created starter project

---
+++ SwiftUICombine/SwiftUICombine/SwiftUICombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineApp.swift
+//  SwiftUICombine
+//
+//  Created by Adel Aref on 27/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUICombine/SwiftUICombineTests/SwiftUICombineTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineTests.swift
+//  SwiftUICombineTests
+//
+//  Created by Adel Aref on 27/11/2024.
+//
+
+import Testing
+@testable import SwiftUICombine
+
+struct SwiftUICombineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
